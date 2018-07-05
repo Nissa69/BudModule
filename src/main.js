5c0919b74c22188516aa54311a7fcfa62ab5c5e8
Nissa69 <yazzz01@gmail.com>
2018-07-05 16:35:49 +0200

    wip webpack

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  entry: 'main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
+
 Vue.config.productionTip = false
 
 new Vue({
